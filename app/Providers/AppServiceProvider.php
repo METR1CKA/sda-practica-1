1ee32594fac4e1753ff63f0f3511d67f46a22669
METR1CKA <ferchosalazar054@gmail.com>
2024-02-26 12:41:15 -0600

    [UPD] v1.7.8
    - Add last changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,83 +23,81 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    if (env('APP_ENV') !== 'production') {
-      // Validar la conexión con la base de datos
-      try {
-        DB::connection()->getPdo();
-      } catch (\Exception $e) {
-        Log::error('CONNECTION TO DATABASE', [
-          'STATUS' => 'ERROR',
-          'MESSAGE' => $e->getMessage(),
-          'LINE_CODE' => $e->getLine(),
-          'ACTION' => 'Exception to connect to database',
-          'CONTROLLER' => AppServiceProvider::class,
-          'METHOD' => 'boot',
-          'TRACE' => $e->getTraceAsString(),
-          'FILE' => $e->getFile(),
-        ]);
+    // // Validar la conexión con la base de datos
+    // try {
+    //   DB::connection()->getPdo();
+    // } catch (\Exception $e) {
+    //   Log::error('CONNECTION TO DATABASE', [
+    //     'STATUS' => 'ERROR',
+    //     'MESSAGE' => $e->getMessage(),
+    //     'LINE_CODE' => $e->getLine(),
+    //     'ACTION' => 'Exception to connect to database',
+    //     'CONTROLLER' => AppServiceProvider::class,
+    //     'METHOD' => 'boot',
+    //     'TRACE' => $e->getTraceAsString(),
+    //     'FILE' => $e->getFile(),
+    //   ]);
 
-        error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
+    //   error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
 
-        exit(1);
-      }
+    //   exit(1);
+    // }
 
-      // Validaciones antes de ejecutar el servidor
-      $running_in_console = app()->runningInConsole();
+    // // Validaciones antes de ejecutar el servidor
+    // $running_in_console = app()->runningInConsole();
 
-      $command = $running_in_console ? $_SERVER['argv'][1] ?? null : null;
+    // $command = $running_in_console ? $_SERVER['argv'][1] ?? null : null;
 
-      if ($running_in_console && $command == 'serve') {
-        // Validar si las migraciones se han ejecutado
-        try {
-          $migrations_pending = Artisan::call('migrate:status') > 0;
+    // if ($running_in_console && $command == 'serve') {
+    //   // Validar si las migraciones se han ejecutado
+    //   try {
+    //     $migrations_pending = Artisan::call('migrate:status') > 0;
 
-          if ($migrations_pending) {
-            throw new \Exception('Migrations have not been executed. Execute \'php artisan migrate\' to run them.');
-          }
-        } catch (\Exception $e) {
-          Log::error('MIGRATIONS HAVE NOT BEEN EXECUTED', [
-            'STATUS' => 'ERROR',
-            'MESSAGE' => $e->getMessage(),
-            'LINE_CODE' => $e->getLine(),
-            'ACTION' => 'Exception to run server',
-            'CONTROLLER' => AppServiceProvider::class,
-            'METHOD' => 'boot',
-            'TRACE' => $e->getTraceAsString(),
-            'FILE' => $e->getFile(),
-          ]);
+    //     if ($migrations_pending) {
+    //       throw new \Exception('Migrations have not been executed. Execute \'php artisan migrate\' to run them.');
+    //     }
+    //   } catch (\Exception $e) {
+    //     Log::error('MIGRATIONS HAVE NOT BEEN EXECUTED', [
+    //       'STATUS' => 'ERROR',
+    //       'MESSAGE' => $e->getMessage(),
+    //       'LINE_CODE' => $e->getLine(),
+    //       'ACTION' => 'Exception to run server',
+    //       'CONTROLLER' => AppServiceProvider::class,
+    //       'METHOD' => 'boot',
+    //       'TRACE' => $e->getTraceAsString(),
+    //       'FILE' => $e->getFile(),
+    //     ]);
 
-          error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
+    //     error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
 
-          exit(1);
-        }
+    //     exit(1);
+    //   }
 
-        // Validar si los seeders se han ejecutado
-        try {
-          $exist_table_roles = Schema::hasTable('roles');
+    //   // Validar si los seeders se han ejecutado
+    //   try {
+    //     $exist_table_roles = Schema::hasTable('roles');
 
-          $exist_data_in_table_roles = DB::table('roles')->exists();
+    //     $exist_data_in_table_roles = DB::table('roles')->exists();
 
-          if ($exist_table_roles && !$exist_data_in_table_roles) {
-            throw new \Exception('Seeders have not been executed. Execute \'php artisan db:seed\' to run them.');
-          }
-        } catch (\Exception $e) {
-          Log::error('SEEDERS HAVE NOT BEEN EXECUTED', [
-            'STATUS' => 'ERROR',
-            'MESSAGE' => $e->getMessage(),
-            'LINE_CODE' => $e->getLine(),
-            'ACTION' => 'Exception to run server',
-            'CONTROLLER' => AppServiceProvider::class,
-            'METHOD' => 'boot',
-            'TRACE' => $e->getTraceAsString(),
-            'FILE' => $e->getFile(),
-          ]);
+    //     if ($exist_table_roles && !$exist_data_in_table_roles) {
+    //       throw new \Exception('Seeders have not been executed. Execute \'php artisan db:seed\' to run them.');
+    //     }
+    //   } catch (\Exception $e) {
+    //     Log::error('SEEDERS HAVE NOT BEEN EXECUTED', [
+    //       'STATUS' => 'ERROR',
+    //       'MESSAGE' => $e->getMessage(),
+    //       'LINE_CODE' => $e->getLine(),
+    //       'ACTION' => 'Exception to run server',
+    //       'CONTROLLER' => AppServiceProvider::class,
+    //       'METHOD' => 'boot',
+    //       'TRACE' => $e->getTraceAsString(),
+    //       'FILE' => $e->getFile(),
+    //     ]);
 
-          error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
+    //     error_log("\n[-] ERROR: " . $e->getMessage() . "\n");
 
-          exit(1);
-        }
-      }
-    }
+    //     exit(1);
+    //   }
+    // }
   }
 }
